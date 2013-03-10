20b543bd9b385189ba0df8c308b1f9c45249dace
cyberfunk <hello@cyberfunk.org>
2013-03-10 23:46:31 +0100

    Add ability for boilerplate.sh to walk parent hierarchy so that nested executables can find the strike.sh script, necessary for the test executables to pass.

--- lib/boilerplate.sh
+++ lib/boilerplate.sh
@@ -1,31 +1,57 @@
 set -o errtrace;
 set -o nounset;
 
+# walk parent hierarchy looking
+# for the script
+walk() {
+  local name="${1:-}";
+  local dir="$2";
+  file="${dir}/${name}";
+  while ! test -f "$file" && [ "$dir" != "/" ];
+    do
+      dir=$( dirname "$dir" );
+      file="${dir}/${name}";
+  done
+}
+
+# source the script
+import() {
+  local script="$1";
+  . "${script}" "$@" 2>/dev/null \
+    || { printf "fatal: failed to load %s\n" \
+      "${script}" && exit 1; };
+}
+
+# resolve the script from a pre-defined
+# set of paths
+resolve() {
+  local base="$1"; shift;
+  local name="strike";
+  local script="${name}.sh";
+  local file="";
+  local paths=(
+    "${base}/../lib/${script}"
+    "${base}/../node_modules/${name}/lib/${script}"
+  );
+  local f;
+  for f in "${paths[@]}"
+    do
+      if [ -f "${f}" ]; then
+        import "${f}"; return 0;
+      fi
+  done
+  walk "lib/${script}" "$base";
+  if [ -f "$file" ]; then
+    import "${file}"; return 0;
+  fi
+  printf "fatal: could not locate %s\n" "${script}"; exit 1;
+}
+
 # resolve executable directory
 # including symbolic links and
 # find strike.sh to source the
 # library code
 boilerplate() {
-  resolve() {
-    local base="$1"; shift;
-    local name="strike";
-    local script="${name}.sh";
-    local paths=(
-      "${base}/../lib/${script}"
-      "${base}/../node_modules/${name}/lib/${script}"
-    );
-    local f;
-    for f in "${paths[@]}"
-      do
-        if [ -f "${f}" ]; then
-          . "${f}" "$@" 2>/dev/null \
-            || { printf "fatal: failed to load %s\n" \
-              "${script}" && exit 1; };
-          return 0;
-        fi
-    done
-    printf "fatal: could not locate %s\n" "${script}"; exit 1;
-  }
   local data=( $( caller 0 ) );
   local src="${data[2]}";
   local exedir="$( dirname "$src" )";
@@ -38,5 +64,9 @@ boilerplate() {
   exedir="$( cd -P "$( dirname "$src" )" && pwd )";
   unset src;
   resolve "$exedir" "$@" \
-    && method.remove $FUNCNAME resolve;
+    && method.remove \
+      boilerplate \
+      resolve \
+      import \
+      walk;
 }
