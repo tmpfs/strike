0b60719ac15f7d00d43e8fbfb721da294c9b682b
cyberfunk <hello@cyberfunk.org>
2013-04-14 19:32:04 +0200

    Refactor delegate module.

--- lib/modules/core/delegate.sh
+++ lib/modules/core/delegate.sh
@@ -1,20 +1,27 @@
-declare -grx delegate_delimiter=".";
+declare -g delegate_delimiter=".";
 delegate() {
-  # method pseudo-namespace delimiter
-  local delimiter="${delegate_delimiter}";
-  local module=${1:-};
-  shift;
-  # TODO: only shift if we have more options to process
-  local method=${1:-};
-  shift;
+  local module=${1:-}; shift;
+  local method=${1:-}; shift;
   #TODO: test whether the delegate method exists
-  if [ ! -z "$method" ]; then
+  local delegate_method_name="${method}";
+  if [ -n "$method" ]; then
+    if [ -n "${module}" ]; then
+      delegate_method_name="${module}${delegate_delimiter}${method}";
+    fi
     #straight method call
-    if [ -z "$module" ]; then
-      "${method}" "$@";
+    #if [ -z "$module" ]; then
+      #"${method}" "$@";
     #method call in module
+    #else
+      #"${module}${delimiter}${method}" "$@";
+    #fi
+
+    unset -v module method;
+    if method.exists? "${delegate_method_name}"; then
+      "${delegate_method_name}" "$@";
     else
-      "${module}${delimiter}${method}" "$@";
+      console error -- "delegate command %s does not exist" \
+        "${delegate_method_name}";
     fi
   fi
 }
